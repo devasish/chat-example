f27282c42d2cee58373b449cc54ed6d08ea9a0ce
devasish <dev.achieve@gmail.com>
2015-10-01 12:00:49 +0530

    Notification and IP mapping added.

--- index.js
+++ index.js
@@ -3,12 +3,43 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
+	console.log(req.connection.remoteAddress);
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+	  var address = socket.handshake.address;
+		var exp = address.split(":");
+		var exp2 = exp[exp.length - 1].split('.');
+		var key = exp2[exp2.length - 1];
+
+		var name = '';
+		switch(key) {
+			case 53 : 
+			case '53' : 
+			name = 'Swarup'; break;
+			case 59 : 
+			case '59' : 
+			name = 'Rajkumar'; break;
+			case 63 : 
+			case '63' :
+			case '1' : 
+			name = 'Devasish'; break;
+			case 64 : 
+			case '64' :
+			name = 'Koustav'; break;
+			case 69 : 
+			case '69' : 
+			name = 'Tanmoy'; break;
+			case 71 : 
+			case '71' : 
+			name = 'Projesh'; break;
+			default:
+			name = 'Anonymous [' + exp2 + ']'
+			
+		}
+    io.emit('chat message', name +" : "+ msg);
   });
 });
 
