6703ea7f990e9308a58f13982c758ba831667900
devasish <dev.achieve@gmail.com>
2016-07-07 19:09:14 +0530

    Editor Added. Editable Div instead of Textbox

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app     = require('express')();
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
 
-app.get('/', function(req, res){
-	console.log(req.connection.remoteAddress);
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    console.log(req.connection.remoteAddress);
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-	  var address = socket.handshake.address;
-		var exp = address.split(":");
-		var exp2 = exp[exp.length - 1].split('.');
-		var key = exp2[exp2.length - 1];
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        var address = socket.handshake.address;
+        var exp = address.split(":");
+        var exp2 = exp[exp.length - 1].split('.');
+        var key = exp2[exp2.length - 1];
 
-		var name = '';
-		switch(key) {
-			case 53 : 
-			case '53' : 
-			name = 'Swarup'; break;
-			case 59 : 
-			case '59' : 
-			name = 'Rajkumar'; break;
-			case 63 : 
-			case '63' :
-			case '1' : 
-			name = 'Devasish'; break;
-			case 64 : 
-			case '64' :
-			name = 'Koustav'; break;
-			case 69 : 
-			case '69' : 
-			name = 'Tanmoy'; break;
-			case 71 : 
-			case '71' : 
-			name = 'Projesh'; break;
-			default:
-			name = 'Anonymous [' + exp2 + ']'
-			
-		}
-    io.emit('chat message', name +" : "+ msg);
-  });
+        var name = '';
+        switch (key) {
+            case 53 :
+            case '53' :
+                name = 'Swarup';
+                break;
+            case 59 :
+            case '59' :
+                name = 'Rajkumar';
+                break;
+            case 63 :
+            case '63' :
+            case '1' :
+                name = 'Devasish';
+                break;
+            case 64 :
+            case '64' :
+                name = 'Koustav';
+                break;
+            case 69 :
+            case '69' :
+                name = 'Tanmoy';
+                break;
+            case 71 :
+            case '71' :
+                name = 'Projesh';
+                break;
+            default:
+                name = 'Anonymous [' + exp2 + ']'
+
+        }
+//        io.emit('chat message', name + " : " + msg);
+        io.emit('chat message', {name : name, msg : msg});
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
