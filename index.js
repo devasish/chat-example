4d50d04f5db236f9078f60e8a5eab5c1bc620475
devasish <dev.achieve@gmail.com>
2016-07-11 19:18:55 +0530

    one to one in progress

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app     = require('express')();
-var http    = require('http').Server(app);
-var io      = require('socket.io')(http);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var users = [
+    
+];
+var socketIdName = {};
 
 app.get('/', function (req, res) {
     console.log(req.connection.remoteAddress);
@@ -8,48 +12,41 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        var address = socket.handshake.address;
-        var exp = address.split(":");
-        var exp2 = exp[exp.length - 1].split('.');
-        var key = exp2[exp2.length - 1];
-
-        var name = '';
-        switch (key) {
-            case 53 :
-            case '53' :
-                name = 'Swarup';
-                break;
-            case 59 :
-            case '59' :
-                name = 'Rajkumar';
-                break;
-            case 63 :
-            case '63' :
-            case '1' :
-                name = 'Devasish';
-                break;
-            case 64 :
-            case '64' :
-                name = 'Koustav';
-                break;
-            case 69 :
-            case '69' :
-                name = 'Tanmoy';
-                break;
-            case 71 :
-            case '71' :
-                name = 'Projesh';
-                break;
-            default:
-                name = 'Anonymous [' + exp2 + ']'
-
+    socket.on('chat message', function (message) {
+        var msg     = message;
+        var from    = msg.from;
+        var to      = msg.to;
+        var body    = msg.body;
+        console.log(msg);
+        if (validUser()) {
+            io.emit('cast' + to, msg)
         }
 //        io.emit('chat message', name + " : " + msg);
-        io.emit('chat message', {name : name, msg : msg});
+        //io.emit('chat message', {name : name, msg : msg});
+        //io.emit('user' + key, {news: "hello world"});
+    });
+    
+    socket.on('new-connection', function(message) {
+        //console.log(socket.id + ' connected');
+        
+        users.push(message.name);
+        socketIdName[socket.id] = message.name;
+        
+        io.emit('new-connection', {users : users});
+    });
+    
+    socket.on('disconnect', function() {
+        var userName = socketIdName[socket.id];
+        users.splice(users.indexOf(userName), 1);
+        io.emit('do-disconnect', {users : users});
+        //remove user from db
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(3030, function () {
+    console.log('listening on *:3030');
 });
+
+function validUser() {
+    return true;
+}
